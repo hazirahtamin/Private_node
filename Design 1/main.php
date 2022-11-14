b0e21a6bbf2d04c860670f398ce8a9d78fefe757
hazirahtamin <95665858+hazirahtamin@users.noreply.github.com>
2022-11-14 16:16:22 +0800

    Add files via upload

---
+++ Design 1/main.php
@@ -0,0 +1,46 @@
+<?php
+
+echo'
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Croffle Shop</title>
+
+        <link href="CSS/main.css" rel="stylesheet"> 
+        
+        <script src="JS/JsLibrary/jquery-3.5.1.js"></script>
+        <script src="JS/JsLibrary/jquery-confirm.min.js"></script>
+
+        <script src="JS/main.js"></script>
+    </head>
+    
+    <body>
+        <div class = "main-img">
+            <img src = "Images/main/img5.jpeg">
+        </div>
+        <div class = "mainBody">
+            <div class = "top-section">
+                <div class = "header-website">
+                    <h1>CROFFLE</h1>
+                </div>
+                <div class = "nav-page">
+                    <button class = "nav-button" >Home</button>
+                    <button class = "nav-button" >Menu</button>
+                    <button class = "nav-button" >Book Table</button>
+                    <button class = "nav-button" >Find Us</button>
+                </div>
+            </div>
+            <div class = "bottom-section">
+                <div class = "page-website">
+                    <div class = "slide-image">
+                        <img src = "Images/main/img7.jpg">
+                        <img src = "Images/main/img5.jpeg">
+                        <img src = "Images/main/img6.jpeg">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>';
+?>
+
